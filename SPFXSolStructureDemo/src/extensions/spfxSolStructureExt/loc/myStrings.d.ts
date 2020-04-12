ebfa1649ff35e2863513c1afb0ee281af8aa2252
Vikita Pawar <vktpawar@gmail.com>
2020-04-12 22:40:46 +0530

    SPFXSolStructureDemo

---
+++ SPFXSolStructureDemo/src/extensions/spfxSolStructureExt/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ISpfxSolStructureExtApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'SpfxSolStructureExtApplicationCustomizerStrings' {
+  const strings: ISpfxSolStructureExtApplicationCustomizerStrings;
+  export = strings;
+}
---
+++ SPFXSolStructureDemo/src/webparts/spfxSolStructureWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxSolStructureWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpfxSolStructureWpWebPartStrings' {
+  const strings: ISpfxSolStructureWpWebPartStrings;
+  export = strings;
+}
